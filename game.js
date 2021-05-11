bba9a94b0e7e002852eec03f8054ee79c9f50363
Georgia Partyka <georgiapartyka@gmail.com>
2021-05-11 19:48:55 -0400

    palette swapping

--- game.js
+++ game.js
@@ -1,30 +1,50 @@
 function update() 
-{
-    
-    updateDragons(gContext,0,0,0,0,0,0)
+{   
+  
+    if(!random){ 
+        updateDragons(gContext,getPalette());
+        random = true;
+    }
 }
 
-function updateDragons(context,redMax,redMin,greenMax,greenMin,blueMax,blueMin)
+function updateDragons(context,palette)
 {
-    var pixels = context.getImageData(0, 0, spriteWidth*scale, spriteHeight*scale);
+    var pixels = context.getImageData(0, 0, spriteWidth, spriteHeight);
 
     for (i = 0; i < pixels.data.length / 4; i++)
     {
-        changePixelColor(pixels.data, i * 4, gray1,76,0,0);
-        changePixelColor(pixels.data, i * 4, gray2,153,21,21);
-        changePixelColor(pixels.data, i * 4, gray3,163,163,36);
+        changePixelColor(pixels.data, i * 4, gray1,palette[0].r,palette[0].g,palette[0].b);
+        changePixelColor(pixels.data, i * 4, gray2,palette[1].r,palette[1].g,palette[1].b);
+        changePixelColor(pixels.data, i * 4, gray3,palette[2].r,palette[2].g,palette[2].b);
         changePixelColor(pixels.data, i * 4, 0,50,50,50);
     }
 
     context.putImageData(pixels, 0, 0);
+    canvas.refresh();
 }
 
-function changePixelColor(data,index,colorToChange,red,blue,green){
+function changePixelColor(data,index,colorToChange,red,green,blue){
 
      if(data[index]==colorToChange){ 
          data[index] =red;
-         data[index + 1] =  blue;
-         data[index + 2] =  green;
+         data[index + 1] =  green;
+         data[index + 2] =  blue;
     }
+}
+
+function changeHorns(){
+    
+}
+
+function getPalette(){
+    var pixels = [];
+            while(pixels.length < 3){
+                var r = Math.floor(Math.random() * paletteWidth);
+                if(!pixels.includes(colorLookup[r])){
+                    pixels.push(colorLookup[r]);
+            }
+        }
+
+    return pixels;
 }
 
--- setup.js
+++ setup.js
@@ -1,43 +1,108 @@
 function preload() 
 {
-    this.load.image('western-plates-base', 'assets/western-plates-base.png');
-    this.load.image('western-koi-base', 'assets/western-koi-base.png');
-    this.load.image('western-spike-tail', 'assets/western-spike-tail.png');
-    this.load.image('western-frond-tail', 'assets/western-frond-tail.png');
-    this.load.image('western-tail-base', 'assets/western-koi-tail-base.png');
-    this.load.image('western-horn1', 'assets/western-horn-1.png');
-    this.load.image('western-horn2', 'assets/western-horn-2.png');
-    this.load.image('western-horn3', 'assets/western-horn-3.png');
-    this.load.image('western-horn3', 'assets/dragonPalette.png');
-    
+    this.load.image('palette', 'assets/dragonPalette.png');
+
+
+    // western
+    this.load.image('western-plates-base', 'assets/WesternAssets/western-plates-base.png');
+    this.load.image('western-giraffe-base', 'assets/WesternAssets/western-giraffe-base.png');
+    this.load.image('western-koi-base', 'assets/WesternAssets/western-koi-base.png');
+    this.load.image('western-tail-spikes', 'assets/WesternAssets/western-tail-spikes.png');
+    this.load.image('western-tail-ball', 'assets/WesternAssets/western-tail-ball.png');
+    this.load.image('western-horn1', 'assets/WesternAssets/western-horn1.png');
+    this.load.image('western-horn2', 'assets/WesternAssets/western-horn2.png');
+    this.load.image('western-horn3', 'assets/WesternAssets/western-horn3.png');
+    this.load.image('western-horn4', 'assets/WesternAssets/western-horn4.png');
+    this.load.image('western-horn5', 'assets/WesternAssets/western-horn5.png');
+    this.load.image('western-horn5', 'assets/WesternAssets/western-horn6.png');
+
+    // hydra
+    this.load.image('hydra-plates-base', 'assets/hydraAssets/hydra-plates-base.png');
+    this.load.image('hydra-giraffe-base', 'assets/hydraAssets/hydra-giraffe-base.png');
+    this.load.image('hydra-koi-base', 'assets/hydraAssets/hydra-koi-base.png');
+    this.load.image('hydra-tail-spikes', 'assets/hydraAssets/hydra-tail-spikes.png');
+    this.load.image('hydra-tail-ball', 'assets/hydraAssets/hydra-tail-ball.png');
+    this.load.image('hydra-horn1', 'assets/hydraAssets/hydra-horn1.png');
+    this.load.image('hydra-horn2', 'assets/hydraAssets/hydra-horn2.png');
+    this.load.image('hydra-horn3', 'assets/hydraAssets/hydra-horn3.png');
+    this.load.image('hydra-horn4', 'assets/hydraAssets/hydra-horn4.png');
+    this.load.image('hydra-horn5', 'assets/hydraAssets/hydra-horn5.png');
+    this.load.image('hydra-horn5', 'assets/hydraAssets/hydra-horn6.png');
+
+    // wyvern 
+    this.load.image('wyvern-plates-base', 'assets/wyvernAssets/wyvern-plates-base.png');
+    this.load.image('wyvern-giraffe-base', 'assets/wyvernAssets/wyvern-giraffe-base.png');
+    this.load.image('wyvern-koi-base', 'assets/wyvernAssets/wyvern-koi-base.png');
+    this.load.image('wyvern-tail-spikes', 'assets/wyvernAssets/wyvern-tail-spikes.png');
+    this.load.image('wyvern-tail-ball', 'assets/wyvernAssets/wyvern-tail-ball.png');
+    this.load.image('wyvern-horn1', 'assets/wyvernAssets/wyvern-horn1.png');
+    this.load.image('wyvern-horn2', 'assets/wyvernAssets/wyvern-horn2.png');
+    this.load.image('wyvern-horn3', 'assets/wyvernAssets/wyvern-horn3.png');
+    this.load.image('wyvern-horn4', 'assets/wyvernAssets/wyvern-horn4.png');
+    this.load.image('wyvern-horn5', 'assets/wyvernAssets/wyvern-horn5.png');
+    this.load.image('wyvern-horn5', 'assets/wyvernAssets/wyvern-horn6.png');
+
 }
 function create() 
 {
-	var newTexture;
-	//var context;
-    
-	let gameTextures = [this.textures.get('western-koi-base').getSourceImage(),
-						this.textures.get('western-spike-tail').getSourceImage(),
-						this.textures.get('western-frond-tail').getSourceImage(),
- 						this.textures.get('western-tail-base').getSourceImage(),
-						this.textures.get('western-horn1').getSourceImage(),
-						this.textures.get('western-horn2').getSourceImage(),
-						this.textures.get('western-horn3').getSourceImage()
+	
+	let westernTextures = [ this.textures.get('western-koi-base').getSourceImage(),
+    						this.textures.get('western-tail-spikes').getSourceImage(),
+    						this.textures.get('western-tail-ball').getSourceImage(),
+    						this.textures.get('western-horn1').getSourceImage(),
+    						this.textures.get('western-horn2').getSourceImage(),
+    						this.textures.get('western-horn3').getSourceImage(),
+                            this.textures.get('western-horn4').getSourceImage(),
+                            this.textures.get('western-horn5').getSourceImage(),
 
-						];
+						  ];
 
-	var westernPlatesBase2;
+    let hydraTextures = [ this.textures.get('hydra-koi-base').getSourceImage(),
+                            this.textures.get('hydra-tail-spikes').getSourceImage(),
+                            this.textures.get('hydra-tail-ball').getSourceImage(),
+                            this.textures.get('hydra-horn1').getSourceImage(),
+                            this.textures.get('hydra-horn2').getSourceImage(),
+                            this.textures.get('hydra-horn3').getSourceImage(),
+                            this.textures.get('hydra-horn4').getSourceImage(),
+                            this.textures.get('hydra-horn5').getSourceImage(),
 
-    var newTexture = this.textures.createCanvas('full_dragon', spriteWidth, spriteHeight);
+                          ];
+
+
+  let wyvernTextures = [     this.textures.get('wyvern-koi-base').getSourceImage(),
+                            this.textures.get('wyvern-tail-spikes').getSourceImage(),
+                            this.textures.get('wyvern-tail-ball').getSourceImage(),
+                            this.textures.get('wyvern-horn1').getSourceImage(),
+                            this.textures.get('wyvern-horn2').getSourceImage(),
+                            this.textures.get('wyvern-horn3').getSourceImage(),
+                            this.textures.get('wyvern-horn4').getSourceImage(),
+                            this.textures.get('wyvern-horn5').getSourceImage(),
+
+                          ];
+
+    canvas = this.textures.createCanvas('full_dragon', spriteWidth, spriteHeight);
 
    
-    gContext = newTexture.getSourceImage().getContext('2d');
+    gContext = canvas.getSourceImage().getContext('2d');
 
-    gameTextures.forEach(loopDraw);
+    wyvernTextures.forEach(loopDraw);
 
-    var westernPlatesBase2 = this.add.image(256, 256, 'full_dragon');
+    this.add.image(256, 256, 'full_dragon');
+    canvas.refresh();
 
+    paletteWidth = game.textures.get('palette').getSourceImage().width;
+
+    for (x = 0; x < paletteWidth; x++) {
+        pixel = game.textures.getPixel(x, 0,'palette');
+        colorLookup.push(pixel);
+    }
 }
 function loopDraw(item,index){
 	gContext.drawImage(item,0,0,spriteWidth*scale,spriteHeight*scale);
 }
+
+//dwarven smoking jacket with 50 gems.
+// beard comb
+// scroll with numbers
+// hookah
+// obsidian sandals
